e953f9d4ed36a55ea602cd5626f1eb4995a6aea9
tnbkrh <2111140037f@gmail.com>
2023-09-30 16:04:21 +0900

    bonus_drinkの修正

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,12 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    bonus = amount / 3
+    reminder = amount % 3
+
+    if (bonus + reminder) < 3
+      return amount + bonus
+    else
+      return amount - reminder + total_count_for(bonus + reminder)
+    end
   end
 end
